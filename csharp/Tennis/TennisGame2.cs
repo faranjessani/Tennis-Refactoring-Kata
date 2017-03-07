a623cf25f07e8dd3560396f5def8d7a495b45406
Faran Jessani <faran@jessani.net>
2017-03-07 13:23:23 -0500

    Quickly refactor tennis game 2

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -1,143 +1,66 @@
+using System.Collections.Generic;
+
 namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
-
-        private string p1res = "";
-        private string p2res = "";
-        private string player1Name;
-        private string player2Name;
+        private readonly string _player1Name;
+        private readonly string _player2Name;
+        private Score _p1Point;
+        private Score _p2Point;
 
         public TennisGame2(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            p1point = 0;
-            this.player2Name = player2Name;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
+        }
+
+        private readonly IDictionary<Score, string> _scores = new Dictionary<Score, string>(4)
+            {
+                {Score.Love, "Love"},
+                {Score.Fifteen, "Fifteen"},
+                {Score.Thirty, "Thirty"},
+                {Score.Forty, "Forty"}
+            };
+
+        internal enum Score
+        {
+            Love = 0,
+            Fifteen = 1,
+            Thirty = 2,
+            Forty = 3
         }
 
         public string GetScore()
         {
             var score = "";
-            if (p1point == p2point && p1point < 3)
-            {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
-            }
-            if (p1point == p2point && p1point > 2)
+            
+            if (IsGameTied && _p1Point <= Score.Thirty)
+                score = _scores[_p1Point] + "-All";
+            else if(IsGameTied && _p1Point > Score.Thirty)
                 score = "Deuce";
+            else if (_p1Point <= Score.Forty && _p2Point <= Score.Forty)
+                score = _scores[_p1Point] + "-" + _scores[_p2Point];
+            else if (_p1Point > Score.Forty && _p1Point - _p2Point >= 2)
+                score = $"Win for {_player1Name}";
+            else if (_p2Point > Score.Forty && _p2Point - _p1Point >= 2)
+                score = $"Win for {_player2Name}";
+            else if (_p1Point > _p2Point && _p2Point >= Score.Forty)
+                score = $"Advantage {_player1Name}";
+            else if (_p2Point > _p1Point && _p1Point >= Score.Forty)
+                score = $"Advantage {_player2Name}";
 
-            if (p1point > 0 && p2point == 0)
-            {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
-            }
             return score;
         }
 
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
-        }
+        private bool IsGameTied => _p1Point == _p2Point;
 
         public void WonPoint(string player)
         {
-            if (player == "player1")
-                P1Score();
+            if (player == _player1Name)
+                _p1Point++;
             else
-                P2Score();
+                _p2Point++;
         }
-
     }
 }
-
