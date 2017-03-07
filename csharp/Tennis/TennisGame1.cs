1daa6b009bb1bf7fe92ee9bb1aa4d4141d579a39
Faran Jessani <faran@jessani.net>
2017-03-06 23:24:38 -0500

    Added the use of a list with sort to reduce if statement nesting

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -1,69 +1,101 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Tennis
 {
+    using static TennisGame1.Score;
+
     internal class TennisGame1 : ITennisGame
     {
-        private readonly string _player1Name;
-        private readonly string _player2Name;
-        private int _player1Score;
-        private int _player2Score;
+        private readonly Player _player1, _player2;
+        private readonly Player[] _sortedPlayers;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            _player1Name = player1Name;
-            _player2Name = player2Name;
+            _player1 = new Player(player1Name);
+            _player2 = new Player(player2Name);
+            _sortedPlayers = new[] {_player1, _player2};
         }
 
         public string GetScore()
         {
-            if (GameIsOver)
-                return _player1Score > _player2Score ? $"Win for {_player1Name}" : $"Win for {_player2Name}";
+            if (GameIsWon)
+            {
+                return $"Win for {_sortedPlayers[0]}";
+            }
+
+            if (AtLeastThreePointsScored)
+            {
+                if (GameIsTied)
+                    return "Deuce";
+                return $"Advantage {_sortedPlayers[0]}";
+            }
+
             if (GameIsTied)
-                return _player1Score >= 3 ? "Deuce" : $"{ScoreDescription.For(_player1Score)}-All";
-            if (Player1HasAdvantage)
-                return $"Advantage {_player1Name}";
-            if (Player2HasAdvantage)
-                return $"Advantage {_player2Name}";
-
-            return $"{ScoreDescription.For(_player1Score)}-{ScoreDescription.For(_player2Score)}";
+                return $"{Description.For(_sortedPlayers[0].Score)}-All";
+            return $"{Description.For(_player1.Score)}-{Description.For(_player2.Score)}";
         }
-
-        private bool GameIsOver => (_player1Score >= 4 || _player2Score >= 4) && Math.Abs(_player1Score - _player2Score) >= 2;
-
-        private bool Player2HasAdvantage => _player2Score > _player1Score && _player2Score >= 4;
-
-        private bool Player1HasAdvantage => _player1Score > _player2Score && _player1Score >= 4;
-
-        private bool GameIsTied => _player1Score == _player2Score;
 
         public void WonPoint(string playerName)
         {
-            if (playerName == _player1Name)
-                _player1Score++;
+            if (playerName.Equals(_sortedPlayers[0].Name, StringComparison.InvariantCultureIgnoreCase))
+                _sortedPlayers[0].Score++;
             else
-                _player2Score++;
+            {
+                _sortedPlayers[1].Score++;
+                if (_sortedPlayers[1].Score <= _sortedPlayers[0].Score) return;
+
+                // Swap order to maintain descending order in list
+                var temp = _sortedPlayers[0];
+                _sortedPlayers[0] = _sortedPlayers[1];
+                _sortedPlayers[1] = temp;
+            }
         }
 
-        internal static class ScoreDescription
+        private bool GameIsWon => _sortedPlayers.Any(p => p.Score > Forty) && Math.Abs(_sortedPlayers[0].Score - _sortedPlayers[1].Score) >= 2;
+
+        private bool GameIsTied => _sortedPlayers[0].Score == _sortedPlayers[1].Score;
+
+        private bool AtLeastThreePointsScored => _sortedPlayers.All(p => p.Score >= Forty);
+
+        internal enum Score
         {
-            private static readonly IDictionary<int, string> Scores = new Dictionary<int, string>(4)
+            Love = 0,
+            Fifteen = 1,
+            Thirty = 2,
+            Forty = 3
+        }
+
+        internal static class Description
+        {
+            private static readonly IDictionary<Score, string> Scores = new Dictionary<Score, string>(4)
             {
-                {0, Love},
-                {1, Fifteen},
-                {2, Thirty},
-                {3, Fourty}
+                {Score.Love, "Love"},
+                {Score.Fifteen, "Fifteen"},
+                {Score.Thirty, "Thirty"},
+                {Score.Forty, "Forty"}
             };
 
-            public static string Love => "Love";
-            public static string Fifteen => "Fifteen";
-            public static string Thirty => "Thirty";
-            public static string Fourty => "Forty";
-
-            public static string For(int score)
+            public static string For(Score score)
             {
                 return Scores[score];
+            }
+        }
+
+        internal class Player
+        {
+            public Player(string name)
+            {
+                Name = name;
+                Score = Love;
+            }
+
+            public string Name { get; set; }
+            public Score Score { get; set; }
+            public override string ToString()
+            {
+                return $"{Name}";
             }
         }
     }
